a9244d838a7cb81b3261956f164a8b3cf317097b
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 04:15:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,8 @@ def monopoly_with_third_tier
     pieces: 4,
     rent_in_dollars: {
       one_piece_owned: 25,
-      one_piece_owned:
+      two_pieces_owned: 50,
+      
     },
     names: {}
   }}
